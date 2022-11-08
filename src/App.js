25908d0d8f3ddbb25e47b433bb50a4d16315d3cc
Jason Dansie <jasondansie@gmail.com>
2022-11-08 10:12:08 +0200

    changes app back to use components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import './App.css';
 import { Component } from 'react';
+import bubbleSort from './bubbleSort';
+
 
 class App extends Component {
 
   state = {
+    message: "no numbers added.",
     search: "",
+    answer: ""
   };
-
+  
+  
   searchHandler = (event) => {
-    this.setState({ search: event.target.value })
+    this.setState({ search: event.target.value});
     console.log(event.target.value);
+    console.log("search: ",search);
   }
 
-  render() {
+  clickHandler = (event) => {
+    event.preventDefault();
+    
+    console.log(message);
+    var array = this.state.search.split(",").map(Number);   
+    this.setState({this.state.message}bubbleSort(array));
+    console.log("answer:", answer);
+  }
+
+  render(){
     return (
       <div>
           <h1>Sort Algorithms</h1>
           <div className='search'>
-            <label htmlFor="search">search</label>
+          <h3>Enter numbers seperated with a comma</h3>
             <input type="text" name="search" onChange={this.searchHandler}></input>
+            <button onClick={this.clickHandler}>Run bubble sort</button>
           </div>
+          <h2>Message: {answer}</h2>
+          
         </div >
     );
-  }
+  }   
 }
 
 export default App;
